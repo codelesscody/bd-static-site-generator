44b18bfde2751f3326dc5e68139b29b0cbaeda4d
codelesscody <cody@codysparks.com>
2025-09-30 15:25:32 -0500

    added leafnode

---
+++ src/leafnode.py
@@ -0,0 +1,11 @@
+Class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict = None):
+        super().__init__(tag, value, children=[], props=props)  # Leaf nodes have no children
+
+    def to_html(self):
+        props_str = self.props_to_html() if self.props else ""
+        if props_str:
+            return f"<{self.tag} {props_str}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+    
