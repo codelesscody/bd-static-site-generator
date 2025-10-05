7d097a5f8238022122e4705b53363565f6d08fc1
codelesscody <cody@codysparks.com>
2025-10-04 21:14:39 -0500

    updated unit tests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
 from textnode import TextNode, TextType
+from parentnode import ParentNode
+from leafnode import LeafNode
+from htmlnode import HTMLNode
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.STRIKETHROUGH:
+        return LeafNode("s", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
+    else:
+        raise ValueError(f"Unsupported TextType: {text_node.text_type}")
 
 def main():
     dummy_textnode = TextNode("This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev")
--- src/parentnode.py
+++ src/parentnode.py
@@ -10,7 +10,7 @@ class ParentNode(HTMLNode):
         if self.children is None:
             raise ValueError("Children cannot be None for ParentNode.")
         props_str = self.props_to_html() if self.props else ""
-        opening_tag = f"<{self.tag} {props_str}>".strip()
+        opening_tag = f"<{self.tag} {props_str}".strip() + ">"
         closing_tag = f"</{self.tag}>"
         children_html = "".join(child.to_html() for child in self.children)
         return f"{opening_tag}{children_html}{closing_tag}"
