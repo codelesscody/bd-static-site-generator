24c6bed1e068517b2a6906885d73d8e503eb064d
codelesscody <cody@codysparks.com>
2025-10-14 15:35:32 -0500

    Smashing bugs in functions after unit testing

--- src/functions.py
+++ src/functions.py
@@ -3,10 +3,10 @@ from textnode import TextNode, TextType
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
-        if isinstance(node, TextNode.TEXT):
+        if node.text_type == TextType.TEXT and delimiter in node.text:
             parts = node.text.split(delimiter)
             # if there are not an even number of delimiters or zero, raise an error
-            if len(parts) % 2 == 0 or len(parts) == 0:
+            if len(parts) % 2 == 0:
                 raise ValueError(f"Unmatched delimiter: {delimiter}")
             for i, part in enumerate(parts):
                 if i % 2 == 0:
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class TextType(Enum):
-    TEXT = "text"
+    TEXT = "TEXT"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
