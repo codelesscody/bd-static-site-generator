40d19470d0697f953031049647b6a2d7f8da3c03
codelesscody <cody@codysparks.com>
2025-10-17 19:23:06 -0500

    added unit tests for regex functions

--- src/test_functions.py
+++ src/test_functions.py
@@ -28,3 +28,9 @@ class TestFunctions(unittest.TestCase):
         ]
         with self.assertRaises(ValueError):
             split_nodes_delimiter(old_nodes, "*", TextType.BOLD)
+
+def test_extract_markdown_images(self):
+    matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
